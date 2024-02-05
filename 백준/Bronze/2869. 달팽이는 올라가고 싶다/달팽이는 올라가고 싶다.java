fd4df053f48d39009ff5301007b61d1e3fac78a6
오 승기 <49009111+BBaeGGom-sk@users.noreply.github.com>
2024-02-05 22:43:33 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 72 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -9,14 +9,14 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Double A = Double.parseDouble(st.nextToken());
-		Double B = Double.parseDouble(st.nextToken());
-		Double V = Double.parseDouble(st.nextToken());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
 
-		Double target = V - B; // 기준값 = 정상 - 하루 내려가는 양 (내려가기전에 정상에 도착했을 경우를 고려)
-		Double dailyUp = A - B; // 하루에 총 올라가는 양 = 올라가는 양 - 내려가는 양
-		int day = (int)Math.ceil(target / dailyUp); // 소요 일 수 = (기준값 / 하루 총 올라가는 양)를 반올림
-													// int로 형변환하여 정수형으로 출력
+		int target = V - B; // 기준값 = 정상 - 하루 내려가는 양 (내려가기전에 정상에 도착했을 경우를 고려)
+		int dailyUp = A - B; // 하루에 총 올라가는 양 = 올라가는 양 - 내려가는 양
+		int day = target / dailyUp; // 소요일 수 계산
+		if(target % dailyUp != 0) day++; // 주의: 나머지 발생 시 올림해서 계산해야함
 
 		System.out.println(day);
 		br.close();
